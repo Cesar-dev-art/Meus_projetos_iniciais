eff6b6ccc2100b4911e2b25668e8c53c822fe143
Cesar-dev-art <cesardossantosfilho1@gmail.com>
2025-11-15 16:27:54 +0000

    pages/

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,6 @@
 function Home() {
-    return <><h1><big>Titulo de alteração</big></h1>
-    <p>Não gosto de pessego</p></>;
+    return <><h1><big>Oi karolzinha</big></h1>
+    <p>Eu sou seu amor? Se sim, diga "amo meu bem"</p></>;
 }
 
 export default Home;
